b3cb09f0f224cf99f64c4ee6b712c0e7b55e18c5
ystmrdk <rattyasta1@gmail.com>
2020-04-15 14:35:30 +0700

    Updates

---
+++ precache-manifest.31fefaaceac9dcde69c1d596505a4221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7564812420fb948c7bb7aaae85991100",
+    "url": "/UTS-Web3/index.html"
+  },
+  {
+    "revision": "5b82b7bd3253c0af20b7",
+    "url": "/UTS-Web3/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2f18768c34449a6864d4",
+    "url": "/UTS-Web3/static/js/2.e5b0d864.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UTS-Web3/static/js/2.e5b0d864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b82b7bd3253c0af20b7",
+    "url": "/UTS-Web3/static/js/main.d9199977.chunk.js"
+  },
+  {
+    "revision": "00b59eb5da906f72bd6e",
+    "url": "/UTS-Web3/static/js/runtime-main.ba7ad82c.js"
+  }
+]);
